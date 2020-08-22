ae04ae155927f8d46b9dd3a7a387160e548ef9e4
narine <narinemkn@gmail.com>
2020-08-22 21:36:56 +0200

    Updates

---
+++ precache-manifest.d948c18cf2870c705f113abe8484834f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240c1f19f26273be95b76adb4a4505bf",
+    "url": "/Covid-19-Reactjs/index.html"
+  },
+  {
+    "revision": "83a3346fa3528f66b477",
+    "url": "/Covid-19-Reactjs/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "1cee519da6b51837bf90",
+    "url": "/Covid-19-Reactjs/static/css/main.6130a6a9.chunk.css"
+  },
+  {
+    "revision": "83a3346fa3528f66b477",
+    "url": "/Covid-19-Reactjs/static/js/2.48028c3f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Covid-19-Reactjs/static/js/2.48028c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cee519da6b51837bf90",
+    "url": "/Covid-19-Reactjs/static/js/main.58ae39af.chunk.js"
+  },
+  {
+    "revision": "79bc8484e7938179bc60",
+    "url": "/Covid-19-Reactjs/static/js/runtime-main.b93f705a.js"
+  },
+  {
+    "revision": "96d9ca72ed1947ba9ca045f265a495bd",
+    "url": "/Covid-19-Reactjs/static/media/covid.96d9ca72.jpeg"
+  }
+]);
